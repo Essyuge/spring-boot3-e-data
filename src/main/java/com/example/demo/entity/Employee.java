ccc79eb948df2de2bea9a8ddcb89b1dc75cc219a
Essyuge <ugeesther@gmail.com>
2025-01-31 21:26:05 +0300

    add CRUD

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,4 +1,4 @@
-package com.example.demo.Entity;
+package com.example.demo.entity;
 
 
 import jakarta.persistence.*;
@@ -61,7 +61,7 @@ public class Employee {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    //toString
     @Override
     public String toString() {
         return "Employee{" +
@@ -71,5 +71,5 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 '}';
     }
-//toString
+
 }
--- src/main/java/com/example/demo/repo/EmployeeRepository.java
+++ src/main/java/com/example/demo/repo/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repo;
 
-import com.example.demo.Entity.Employee;
+import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "employ")
 public interface EmployeeRepository extends JpaRepository <Employee, Integer>{
 
 }
